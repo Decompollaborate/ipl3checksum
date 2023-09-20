a7ad2da35da4ba1f46d5e1577a17174f94635bb6
angie <angheloalf95@gmail.com>
2023-09-20 12:04:36 -0300

    More tests

---
+++ tests/check_recursive.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2023 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import argparse
+import ipl3checksum
+from pathlib import Path
+import struct
+
+
+print(f"Running ipl3checksum version {ipl3checksum.__version__}")
+
+def checkChecksum(romPath: Path) -> bool:
+    print(romPath)
+
+    print("    Reading...")
+    romBytes = romPath.read_bytes()
+
+    binChecksum = struct.unpack_from(f">II", romBytes, 0x10)
+
+    print(f"    Expected checksum is: 0x{binChecksum[0]:08X} 0x{binChecksum[1]:08X}")
+
+    print("    Detecting CIC...")
+    cicKind = ipl3checksum.detectCIC(romBytes)
+    if cicKind is None:
+        print(f"    Not able to detect CIC for {romPath}")
+        return False
+
+    print("    Calculating checksum...")
+    calculatedChecksum = ipl3checksum.calculateChecksum(romBytes, cicKind)
+    if calculatedChecksum is None:
+        print(f"    Not able to calculate checksum for {romPath}")
+        return False
+
+    print(f"    Calculated checksum is: 0x{calculatedChecksum[0]:08X} 0x{calculatedChecksum[1]:08X}")
+
+    print("    Checking checksum...")
+    if calculatedChecksum[0] != binChecksum[0] or calculatedChecksum[1] != binChecksum[1]:
+        print(f"    Wrong checksum for {romPath}")
+        return False
+
+    print(f"    {romPath} OK")
+
+    return True
+
+def recursePaths(folder: Path) -> int:
+    errors = 0
+
+    for subpath in sorted(folder.iterdir()):
+        if subpath.is_dir():
+            recursePaths(subpath)
+            continue
+
+        if subpath.parts[-2] == "drmario64" and subpath.name == "baserom.cn.z64":
+            # iQue has a wrong checksum for some reason
+            continue
+
+        ok = checkChecksum(subpath)
+        if not ok:
+            errors += 1
+
+    print(f"Total errors: {errors}")
+
+    return errors
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path")
+
+args = parser.parse_args()
+
+exit(recursePaths(Path(args.path)))
