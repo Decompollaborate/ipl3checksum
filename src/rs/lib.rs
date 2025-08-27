39843a1311f51dc0c5bf401a247e76de5475ee67
Anghelo Carvajal <angheloalf95@gmail.com>
2025-08-27 18:17:28 -0400

    Update dependencies

--- src/rs/lib.rs
+++ src/rs/lib.rs
@@ -22,70 +22,73 @@ pub use error::*;
 mod python_bindings {
     use pyo3::prelude::*;
 
+    use super::*;
+
     #[pymodule]
-    fn ipl3checksum(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    fn ipl3checksum(m: &Bound<'_, PyModule>) -> PyResult<()> {
         // Classes
-        m.add_class::<super::cickinds::CICKind>()?;
+        m.add_class::<cickinds::CICKind>()?;
 
         // Free functions
         m.add_function(wrap_pyfunction!(
-            super::checksum::python_bindings::calculateChecksum,
+            checksum::python_bindings::calculateChecksum,
             m
         )?)?;
         m.add_function(wrap_pyfunction!(
-            super::checksum::python_bindings::calculateChecksumAutodetect,
+            checksum::python_bindings::calculateChecksumAutodetect,
             m
         )?)?;
         m.add_function(wrap_pyfunction!(
-            super::detect::python_bindings::detectCICRaw,
+            detect::python_bindings::detectCICRaw,
             m
         )?)?;
         m.add_function(wrap_pyfunction!(
-            super::detect::python_bindings::detectCIC,
+            detect::python_bindings::detectCIC,
             m
         )?)?;
 
         // Exceptions
 
-        register_exceptions_module(py, m)?;
+        register_exceptions_module(m)?;
 
         Ok(())
     }
 
-    fn register_exceptions_module(py: Python<'_>, parent_module: &PyModule) -> PyResult<()> {
+    fn register_exceptions_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
+        let py = parent_module.py();
         let child_module = PyModule::new(py, "exceptions")?;
 
         child_module.add(
             "Ipl3ChecksumError",
-            py.get_type::<super::error::python_bindings::Ipl3ChecksumError>(),
+            py.get_type::<error::python_bindings::Ipl3ChecksumError>(),
         )?;
 
         child_module.add(
             "UnalignedRead",
-            py.get_type::<super::error::python_bindings::UnalignedRead>(),
+            py.get_type::<error::python_bindings::UnalignedRead>(),
         )?;
         child_module.add(
             "ByteConversion",
-            py.get_type::<super::error::python_bindings::ByteConversion>(),
+            py.get_type::<error::python_bindings::ByteConversion>(),
         )?;
         child_module.add(
             "OutOfBounds",
-            py.get_type::<super::error::python_bindings::OutOfBounds>(),
+            py.get_type::<error::python_bindings::OutOfBounds>(),
         )?;
         child_module.add(
             "BufferNotBigEnough",
-            py.get_type::<super::error::python_bindings::BufferNotBigEnough>(),
+            py.get_type::<error::python_bindings::BufferNotBigEnough>(),
         )?;
         child_module.add(
             "BufferSizeIsWrong",
-            py.get_type::<super::error::python_bindings::BufferSizeIsWrong>(),
+            py.get_type::<error::python_bindings::BufferSizeIsWrong>(),
         )?;
         child_module.add(
             "UnableToDetectCIC",
-            py.get_type::<super::error::python_bindings::UnableToDetectCIC>(),
+            py.get_type::<error::python_bindings::UnableToDetectCIC>(),
         )?;
 
-        parent_module.add_submodule(child_module)?;
+        parent_module.add_submodule(&child_module)?;
         Ok(())
     }
 }
